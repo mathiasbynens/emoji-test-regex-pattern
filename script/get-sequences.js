391ae244aab1db2599f43bb4ad94df71aff2d5c6
Mathias Bynens <mathias@qiwi.be>
2021-03-17 16:49:57 +0100

    Attempt to work around regexgen issue
    Issue: #1
    Issue: https://github.com/devongovett/regexgen/issues/31

--- script/get-sequences.js
+++ script/get-sequences.js
@@ -1,7 +1,18 @@
 const getSequences = (packageName) => {
-  const sequences = require(`${packageName}/Sequence_Property/Emoji_Test/index.js`)
-    // https://github.com/devongovett/regexgen/issues/31
-    .sort();
+  const sequences = require(`${packageName}/Sequence_Property/Emoji_Test/index.js`);
+  sequences.sort((a, b) => {
+    // Longest strings first.
+    if ([...a].length > [...b].length) {
+      return -1;
+    }
+    if ([...a].length < [...b].length) {
+      return 1;
+    }
+    // Lengths are equal; sort lexicographically from a-z.
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+  });
   return sequences;
 };
 
