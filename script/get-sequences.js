a00194449f358610b94f1d010a6d5b4be86705af
Mathias Bynens <mathias@qiwi.be>
2021-09-23 16:41:59 +0200

    Add more overqualified iOS emoji sequences
    Issue: #9

--- script/get-sequences.js
+++ script/get-sequences.js
@@ -2,8 +2,6 @@
 // expression pattern, even if they’re not yet in emoji-test.txt.
 // TODO: Remove this special-casing once these sequences land upstream.
 const EXTRA_SEQUENCES = [
-  '\u2615\uFE0F', // hot beverage, as entered via iOS emoji picker
-
   '\u{1F91D}\u{1F3FB}', // handshake: light skin
   '\u{1F91D}\u{1F3FC}', // handshake: medium-light skin
   '\u{1F91D}\u{1F3FD}', // handshake: medium skin
@@ -15,6 +13,38 @@ const EXTRA_SEQUENCES = [
   '\u{1F93C}\u{1F3FD}', // wrestlers: medium skin
   '\u{1F93C}\u{1F3FE}', // wrestlers: medium-dark skin
   '\u{1F93C}\u{1F3FF}', // wrestlers: dark skin
+
+  // Overqualified emoji sequences as entered via the iOS emoji picker.
+  '\u231A\uFE0F', // watch
+  '\u231B\uFE0F', // hourglass
+  '\u25FE\uFE0F', // black medium small square
+  '\u2614\uFE0F', // umbrella with rain drops
+  '\u2615\uFE0F', // hot beverage
+  '\u2648\uFE0F', // Aries
+  '\u2649\uFE0F', // Taurus
+  '\u264A\uFE0F', // Gemini
+  '\u264B\uFE0F', // Cancer
+  '\u264C\uFE0F', // Leo
+  '\u264D\uFE0F', // Virgo
+  '\u264E\uFE0F', // Libra
+  '\u264F\uFE0F', // Scorpius
+  '\u2650\uFE0F', // Sagittarius
+  '\u2651\uFE0F', // Capricorn
+  '\u2652\uFE0F', // Aquarius
+  '\u2653\uFE0F', // Pisces
+  '\u267F\uFE0F', // wheelchair symbol
+  '\u26AA\uFE0F', // medium white circle
+  '\u26BD\uFE0F', // soccer ball
+  '\u26BE\uFE0F', // baseball
+  '\u26C4\uFE0F', // snowman without snow
+  '\u26F2\uFE0F', // fountain
+  '\u26F3\uFE0F', // flag in hole
+  '\u26F5\uFE0F', // sailboat
+  '\u26FA\uFE0F', // tent
+  '\u2B1B\uFE0F', // black large square
+  '\u2B1C\uFE0F', // white large square
+  '\u2B55\uFE0F', // heavy large circle
+  '\u{1F004}\uFE0F', // mahjong tile red dragon
 ];
 
 const getSequences = (packageName) => {
