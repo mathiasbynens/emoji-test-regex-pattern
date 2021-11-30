aecbd0f40de5ae12f61e9246993a42c5a93bad06
Mathias Bynens <mathias@qiwi.be>
2021-11-29 21:12:57 -0300

    Add tests that check no files are empty

--- test/test.js
+++ test/test.js
@@ -15,16 +15,42 @@ const getPackageIdsToCheck = () => {
   return pkgIds;
 };
 
+const assertNotEmpty = (path) => {
+  const contents = fs.readFileSync(path).toString().trim();
+  assert(contents.length > 0);
+};
+
 const checkPackage = (pkgId) => {
   console.log(`Checking ${pkgId}…`);
-  const path = `./dist/${pkgId.replace('unicode-', '')}/javascript.txt`;
-  const pattern = fs.readFileSync(path).toString().trim();
-  const re = new RegExp(pattern);
-  const sequences = getSequences(pkgId);
-  for (const string of sequences) {
-    const actual = string.match(re)[0];
-    assert(string === actual);
+  const prefix = `./dist/${pkgId.replace('unicode-', '')}`;
+
+  {
+    const path = `${prefix}/javascript.txt`;
+    const pattern = fs.readFileSync(path).toString().trim();
+    const re = new RegExp(pattern);
+
+    const sequences = getSequences(pkgId);
+    for (const string of sequences) {
+      const actual = string.match(re)[0];
+      assert(string === actual);
+    }
   }
+
+  {
+    const path = `${prefix}/javascript-u.txt`;
+    const pattern = fs.readFileSync(path).toString().trim();
+    const re = new RegExp(pattern, 'u');
+    const sequences = getSequences(pkgId);
+    for (const string of sequences) {
+      const actual = string.match(re)[0];
+      assert(string === actual);
+    }
+  }
+
+  assertNotEmpty(`${prefix}/index.txt`);
+  assertNotEmpty(`${prefix}/cpp-re2.txt`);
+  assertNotEmpty(`${prefix}/css.txt`);
+  assertNotEmpty(`${prefix}/java.txt`);
 };
 
 const pkgIds = getPackageIdsToCheck();
