49f4d4856e69984488f3721d73d3eb70e1b76565
Mathias Bynens <mathias@qiwi.be>
2022-04-01 08:41:21 +0200

    Tweak shell script

--- script/rebuild.sh
+++ script/rebuild.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-for infile in dist/*/sequences.txt;
-do
-	dir="${infile%/*}"
-	cmd="vendor/bin/build-regexp --infile $infile --overwrite"
+for infile in dist/*/sequences.txt; do
+	dir="${infile%/*}";
+	cmd="vendor/bin/build-regexp --infile $infile --overwrite";
 
-	$cmd --preset java       --outfile "$dir/java.txt"
-	$cmd --preset javascript --outfile "$dir/javascript.txt"
-	$cmd --preset javascript --outfile "$dir/javascript-u.txt" --flags u
-	$cmd --preset re2        --outfile "$dir/cpp-re2.txt"
-done
+	$cmd --preset java       --outfile "$dir/java.txt";
+	$cmd --preset javascript --outfile "$dir/javascript.txt";
+	$cmd --preset javascript --outfile "$dir/javascript-u.txt" --flags u;
+	$cmd --preset re2        --outfile "$dir/cpp-re2.txt";
+done;
